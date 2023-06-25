6e8d19b9b8f7a509b2d1b89bb18b1e81c1d0cc66
anishazahan <anishazahan13@gmail.com>
2023-06-25 16:49:41 +0600

    speaker section update

--- src/pages/components/FeatureSection/FeatureSection.jsx
+++ src/pages/components/FeatureSection/FeatureSection.jsx
@@ -10,7 +10,7 @@ const FeatureSection = () => {
   return (
     <> 
     <div className="">
-        <div className="max-w-6xl mx-auto my-24">
+        <div className="max-w-6xl px-5 md:px-10 lg:px-5 xl:px-0 mx-auto my-24">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-7 gap-y-10">
                 <div className="flex flex-col justify-center  ">
                 <p className="uppercase tracking-[5px] ">features</p>
